d71de7621f7468e5d3a7dea8ebe781deb5b151d1
Queue-ri <qriositylog@gmail.com>
2022-05-06 22:03:45 +0900

    style: Add signin and post button in navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,7 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
+// import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
@@ -13,48 +13,55 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import Searchbar from './Searchbar.js';
 import { useNavigate } from 'react-router-dom';
+import Link from '@mui/material/Link';
+import { Grid } from "@mui/material";
 
-const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Login'];
+// const pages = ['Products', 'Pricing', 'Blog'];
+//const settings = ['Setting', 'Login'];
+const settings = ['Setting'];
 
 const ResponsiveAppBar = () => {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  // const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
   let navigate = useNavigate();
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
+  // const handleOpenNavMenu = (event) => {
+  //   setAnchorElNav(event.currentTarget);
+  // };
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
   };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+  // const handleCloseNavMenu = () => {
+  //   setAnchorElNav(null);
+  // };
 
   const handleCloseUserMenu = (event) => {
     console.log(event.target.textContent);
-    if (event.target.textContent == 'Login') {
+    if (event.target.textContent === 'Login') {
       navigate('/login');
     }
     setAnchorElUser(null);
   };
+  
+  const homeClick = () => {
+    navigate('/');
+  };
+
+  const onClickWrite = () => {
+    navigate('/write');
+  };
+
+  const onClickLogin = () => {
+    navigate('/login');
+  };
 
   return (
-    <AppBar position="static">
+    <AppBar position="sticky">
       <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
-          >
-            Algorio
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+        <Toolbar disableGutters sx={{ display: 'flex' }}>
+        {/**
+        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -90,16 +97,21 @@ const ResponsiveAppBar = () => {
               ))}
             </Menu>
           </Box>
-          
+          */}
+
           <Typography
             variant="h6"
             noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+            component={Link}
+            onClick={homeClick}
+            sx={{ overflow: 'visible', mr: 2, display: 'flex',  color: '#ffffff', textDecoration: 'none' }}
           >
             Algorio
           </Typography>
 
+          
+          
+          {/*
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
               <Button
@@ -111,12 +123,28 @@ const ResponsiveAppBar = () => {
               </Button>
             ))}
           </Box>
+          */}
 
-          <Box sx={{ mr: '20px' }}>
+          <Box sx={{flexGrow: 1 }}>
               <Searchbar />
           </Box>
-          
-          <Box sx={{ flexGrow: 0 }}>
+          <Box justifyContent="right" sx={{flexGrow: 3 }}>
+          <Grid sx={{ paddingRight: 3, justifyContent:'right', flexWrap: 'nowrap' }} container spacing={2} >
+            <Grid item write sx={{flexShrink: 0 }}>
+              <Button variant="contained" onClick={onClickWrite} sx={{textTransform:'none', borderRadius: '0.375rem'}}>Post</Button>
+            </Grid>
+            <Grid item write sx={{flexShrink: 0 }}>
+              <Button variant="outlined" onClick={onClickLogin} sx={{textTransform:'none', color: '#ffffff', borderColor: '#ffffff', borderWidth: '1px', borderRadius: '0.375rem'}}>Sign in</Button>
+            </Grid>
+            {/*
+            <Grid item view sx={{flexShrink: 0 }}>
+              <Button variant="contained" onClick={onClickView}>글보기</Button>
+            </Grid>
+            */}
+          </Grid>
+          </Box>
+
+          <Box justifyContent="right">
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
               <Avatar src="/broken-image.jpg" />
