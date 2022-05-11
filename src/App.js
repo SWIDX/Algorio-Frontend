c87ee3d3ae1e00ffcab57ab23f18e38e26cebef8
Queue-ri <qriositylog@gmail.com>
2022-05-11 22:41:36 +0900

    style(feat): Add post write, view, edit pages to react router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Login.js';
 import Navbar from './Navbar.js'
 import Posts from './Posts.js'
+import PostWrite from './PostWrite.js'
+import PostEdit from './PostEdit.js'
+import PostView from './PostView.js'
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Routes>
           <Route path="/" element={<div><Navbar /><Posts /></div>} />
           <Route path="/login" element={<Login />} />
+          <Route path="/write" element={<div><Navbar /><PostWrite /></div>} />
+          <Route path="/edit" element={<div><Navbar /><PostEdit /></div>} />
+          <Route path="/view/*" element={<div><Navbar /><PostView /></div>} />
         </Routes>
       </div>
     </BrowserRouter>
